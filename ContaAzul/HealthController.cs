a72602b43584d274823bb3c6a91039468fc4f041
William Machado Costa <williammachadocosta.trabalho@gmail.com>
2025-05-01 09:41:36 -0300

    adicionando healthcheck

---
+++ ContaAzul/HealthController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContaAzul
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Healthy");
+        }
+    }
+}
--- ContaAzul/Program.cs
+++ ContaAzul/Program.cs
@@ -4,17 +4,20 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
 
 app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers(); 
 
 var summaries = new[]
 {
